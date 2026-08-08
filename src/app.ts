df6dcb71066c266ef0efc7e17a53293cddf8633d
Jannatul Ferdoush <jannatferdoush1212@gmail.com>
2026-08-08 23:51:52 +0600

    all primary route declared

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,13 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import { authRoute } from "./modules/auth/auth.route";
+import { adminRoute } from "./modules/admin/admin.route";
+import { reviewRoute } from "./modules/review/review.route";
+import { paymentRoute } from "./modules/payment/payment.route";
+import { rentalRoute } from "./modules/rentalRequest/rentalRequest.route";
+import { landlordRoute } from "./modules/landlord/landlord.route";
+import { propertyRoute } from "./modules/property/property.route";
 
 const app: Application = express();
 
@@ -24,5 +31,13 @@ app.use(cookieParser());
 app.get("/", async (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+app.use("/api/auth", authRoute);
+app.use("/api/properties", propertyRoute);
+app.use("/api/landlord", landlordRoute);
+app.use("/api/rentals", rentalRoute);
+app.use("/api/payments", paymentRoute);
+app.use("/api/reviews", reviewRoute);
+app.use("/api/admin", adminRoute);
 
 export default app;
